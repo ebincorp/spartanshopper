c7226ff44e9e65c9f8631de33fe868e3355e0730
James Rasmussen <ebincorp3@gmail.com>
2026-04-11 16:32:11 -0700

    feat: add Google Analytics 4 component scaffold

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -45,6 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} flex flex-col min-h-screen bg-gray-50`}>
+        <GoogleAnalytics />
         <Navbar />
         <div className="flex-1">
           {children}
