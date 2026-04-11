cf15b60b2603acf6ae2cf7a31868d0f11530bdc7
James Rasmussen <ebincorp3@gmail.com>
2026-04-11 12:07:38 -0700

    fix: add sanity.cli.ts and set basePath to /studio in config

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'eohdr7jw',
+    dataset: 'production'
+  }
+})
--- sanity.config.ts
+++ sanity.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 
   projectId: 'eohdr7jw',
   dataset: 'production',
+  basePath: '/studio',
 
   plugins: [structureTool(), visionTool()],
 
